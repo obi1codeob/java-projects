62eb27fccbc2a19dbb350fed38a1e9e9090583f1
obi1codeob <ob1codeob@gmail.com>
2025-09-11 01:56:18 +0000

    at the end of each iterations subtract one from bottlesNum
    created conditional statement to display one message if there are still bottles left to count, and another if there are no more bottles to count (i.e. bottleNum == 0).

--- Head_First_Projects/Ch1/ninetyninebottles.java
+++ Head_First_Projects/Ch1/ninetyninebottles.java
@@ -1,4 +1,5 @@
 package Head_First_Projects.Ch1;
+
 public class ninetyninebottles {
     public static void main(String[] args) {
 
@@ -8,11 +9,19 @@ public class ninetyninebottles {
         while (bottlesNum > 0) {
             if (bottlesNum == 1) {
                 container = "bottle";
-            }   
+            }
             System.out.println(bottlesNum + " " + container + " of beer on the wall,");
             System.out.println(bottlesNum + " " + container + " of beer,");
             System.out.println("Take one down,");
             System.out.println("Pass it around,");
+
+            bottlesNum = bottlesNum - 1;
+            if (bottlesNum > 0) {
+                System.out.println(bottlesNum + " " + container + " of beer on the wall.");
+                System.out.println();
+            } else {
+                System.out.println("No more bottles of beer on the wall.");
+            }
         }
     }
 }
