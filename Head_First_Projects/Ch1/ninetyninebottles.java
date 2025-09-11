bb411c2cfd59dbcad6991fd46f81710250d769e3
obi1codeob <ob1codeob@gmail.com>
2025-09-11 01:53:14 +0000

    declared variables for number of bottles and string "container" for the word "bottles".
    started while loop to run as long as the number of bottles is greater than 0.
    
    added if statement to have the container value "bottle" of the number of bottles is "1".

---
+++ Head_First_Projects/Ch1/ninetyninebottles.java
@@ -0,0 +1,14 @@
+package Head_First_Projects.Ch1;
+public class ninetyninebottles {
+    public static void main(String[] args) {
+
+        int bottlesNum = 99;
+        String container = "bottles";
+
+        while (bottlesNum > 0) {
+            if (bottlesNum == 1) {
+                container = "bottle";
+            }   
+        }
+    }
+}
