f99816317ebb33534865942d7a0acd6112fc7acc
ob1codeob <ob1codeob@gmail.com>
2025-09-28 11:45:44 -0700

    declared variables for age, weight, heartRate, and time to hold values provided by user input
    declared variable calories to hold formula for calculating calories burned

--- calorie_burn_calculator/calorie_burn_calculator.java
+++ calorie_burn_calculator/calorie_burn_calculator.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 package calorie_burn_calculator;
 
 public class calorie_burn_calculator {
     public static void main (String [] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        int weight = sc.nextInt();
+        int heartRate = sc.nextInt();
+        int time = sc.nextInt();
+
+        double calories = ( (age * 0.2757) + (weight * 0.03295) + (heartRate * 1.0781) - 75.4991 ) * time / 8.368;
 
 
     }
