f7882846d3e027caa4a8d722355e772c050cf171
ob1codeob <ob1codeob@gmail.com>
2025-09-28 12:44:13 -0700

    added prompt for user input for operation and numbers to calculate
    add if statement and output for addition of numbers

--- simple_calculator/simple_calc.java
+++ simple_calculator/simple_calc.java
@@ -8,15 +8,37 @@ public class simple_calc {
         // Get user input
         Scanner sc = new Scanner(System.in);
 
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        System.out.println("Welcome to Calc 2025");
+        System.out.println();
 
         //Choose operation
+        System.out.println("Choose and operation: add, subtract, multiply, or divide.");
 
-        String add = sc.nextLine();
-        String sub = sc.nextLine();
-        String mul = sc.nextLine();
-        String div = sc.nextLine();
-        
+        String add = sc.next();
+//        String subtract = sc.next();
+//        String multiply = sc.next();
+//        String divide = sc.next();
+
+        if  (add.equals("add")) {
+            //Get first number
+            System.out.println("Enter your first number.");
+            int num1 = sc.nextInt();
+
+            //Get second number
+            System.out.println("Enter your second number.");
+            int num2 = sc.nextInt();
+
+            int sum = num1 + num2;
+            System.out.println(num1 + " + " + num2 + " equals " + sum);
+        }
+
+
+
+
+
+
+        //Output result
+
+
     }
 }
