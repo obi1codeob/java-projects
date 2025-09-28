5bd8f30143d2d991a09e1915c0ab7c3993ba1e51
ob1codeob <ob1codeob@gmail.com>
2025-09-28 12:24:39 -0700

    declared variables for two numbers to get from user input
    declared variabled for math operation selected by user

---
+++ simple_calculator/simple_calc.java
@@ -0,0 +1,22 @@
+package simple_calculator;
+
+import java.util.Scanner;
+
+public class simple_calc {
+    public static void main(String[] args) {
+
+        // Get user input
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        //Choose operation
+
+        String add = sc.nextLine();
+        String sub = sc.nextLine();
+        String mul = sc.nextLine();
+        String div = sc.nextLine();
+        
+    }
+}
