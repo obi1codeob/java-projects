5d0156f3818cf5c5483a32a209b75d2e58a1726a
ob1codeob <ob1codeob@gmail.com>
2025-09-29 16:55:31 -0700

    declared variables to store user input (coins)

--- counting_the_till/counting_the_till.java
+++ counting_the_till/counting_the_till.java
@@ -1,4 +1,17 @@
 package counting_the_till;
 
+import java.util.Scanner;
+
 public class counting_the_till {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int quarters = sc.nextInt();
+        int dimes = sc.nextInt();
+        int nickels = sc.nextInt();
+        int pennies = sc.nextInt();
+
+
+    }
 }
