0340f969a994eed74aa4700a3728b72596a1c6a6
ob1codeob <ob1codeob@gmail.com>
2025-09-29 18:02:17 -0700

    initial commit
    added program greeting
    
    declared variables for storing user age and formulas for calculating user age in months, weeks, days, hours, minutes and seconds

---
+++ act_your_age/act_your_age.java
@@ -0,0 +1,24 @@
+package act_your_age;
+
+import java.util.Scanner;
+
+public class act_your_age {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Act Your Age, the calculator that takes your age in years, and outputs how old your are in months, weeks, days, hours, minutes, and seconds.");
+        System.out.println();
+
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+
+        int months = age * 12;
+        int weeks = age * 52;
+        int days = age * 365;
+        int hours = age * 24;
+        int minutes = age * 60;
+        int seconds = age * 60 * 60;
+
+    }
+}
