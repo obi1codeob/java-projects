c2b6b3adc3c5ef5831f12e7cfb245b8422f11de4
ob1codeob <ob1codeob@gmail.com>
2025-09-29 17:21:59 -0700

    intial commit
    declared variables to store user input

---
+++ car_lease_calculator/car_lease_calculator.java
@@ -0,0 +1,16 @@
+package car_lease_calculator;
+
+import java.util.Scanner;
+
+public class car_lease_calculator {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int downPayment;
+        int paymentPerMonth;
+        int numMonths;
+        int totalCost;
+
+    }
+}
