ea8e94179c134a968d5a00a7349b8f97b500d0f8
ob1codeob <ob1codeob@gmail.com>
2025-09-30 19:03:17 -0700

    declared variables for user input

---
+++ login_generator/login_generator.java
@@ -0,0 +1,18 @@
+package login_generator;
+
+import java.util.Scanner;
+
+public class login_generator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //Get input from user
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+        int number = sc.nextInt();
+
+
+
+    }
+}
