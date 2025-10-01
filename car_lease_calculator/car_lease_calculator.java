88747f7a4207d0ba09522e5b9ef68274a43eb099
ob1codeob <ob1codeob@gmail.com>
2025-09-30 19:00:15 -0700

    update

--- car_lease_calculator/car_lease_calculator.java
+++ car_lease_calculator/car_lease_calculator.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class car_lease_calculator {
-    public static void main(String args[]){
+    public static void main (String[] args){
 
         Scanner sc = new Scanner(System.in);
 
