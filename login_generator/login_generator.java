639add5db80ba440727272cc4d63581f2d1e89a5
ob1codeob <ob1codeob@gmail.com>
2025-09-30 19:20:47 -0700

    created process to build the login username

--- login_generator/login_generator.java
+++ login_generator/login_generator.java
@@ -12,6 +12,16 @@ public class login_generator {
         String lastName = sc.nextLine();
         int number = sc.nextInt();
 
+        //Build login name
+        //If the length of firstName is greater than 6, take the first 6 characters. Otherwise, use the full firstName and store it in a variable
+        String loginFirst = firstName.length() > 6 ? firstName.substring(0, 6) : firstName;
+
+        //Get the first letter of the last name
+        String loginLast = lastName.substring(0,1);
+
+        //Get the last digit of the number
+        int lastDigit = number % 10;
+
 
 
     }
