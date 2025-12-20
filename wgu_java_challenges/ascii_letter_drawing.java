7e89a2810a63039dbe891e707448f7e98d5007ac
obi1codeob <ob1codeob@gmail.com>
2025-12-20 17:54:50 +0000

    first function: output letter H using print

---
+++ wgu_java_challenges/ascii_letter_drawing.java
@@ -0,0 +1,26 @@
+//PROGRAM: ASCII Letter Drawing
+
+// OBJECTIVE: Write a program that outputs the pattern shown beow, ending with a newine. Each ine of the pattern contains 5 characters including whitespace.
+
+// The output is:**
+
+/*H       H
+    H       H
+    HHHHH
+    H       H
+    H       H
+*/
+
+import java.util.Scanner;
+
+public class ascii_letter_drawing {
+    public static void main(String[] args) {
+
+        //FUNCTION 1: Use print function to output letter pattern
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println("HHHHH");
+        System.out.println("H   H");
+        System.out.println("H   H");
+    }
+}
