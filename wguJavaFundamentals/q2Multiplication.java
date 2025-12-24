c52cf3bc62ee2205916091a826b9e80f349551f8
ob1codeob <ob1codeob@gmail.com>
2025-12-24 15:20:33 -0800

    compute product of two number input by user

---
+++ wguJavaFundamentals/q2Multiplication.java
@@ -0,0 +1,28 @@
+package wguJavaFundamentals;
+
+//Import scanner utility to collect user input
+import java.util.Scanner;
+
+public class q2Multiplication {
+    public static void main(String[] args) {
+
+        //Create new scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Declare variables
+        int starting_num;
+        int multiplier;
+        int product;
+
+        //Get starting num from user
+        System.out.print("Enter a starting number: ");
+        starting_num = sc.nextInt();
+
+        //Get multiplier from user
+        System.out.print("Enter a multiplier: ");
+        multiplier = sc.nextInt();
+
+        //Compute the product
+        product = starting_num * multiplier;
+    }
+}
