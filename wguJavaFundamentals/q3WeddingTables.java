3e8e727914a6692335ff43aaa6c78a8bc7c14bb6
ob1codeob <ob1codeob@gmail.com>
2025-12-24 15:43:20 -0800

    get user input for number of guests and calculate tables filled using integer division

---
+++ wguJavaFundamentals/q3WeddingTables.java
@@ -0,0 +1,59 @@
+//Project: Wedding Tables
+
+/* Objective: Write a program that calculates the number of full tables for a wedding event, based on the number of expected guests.
+
+Each full table will seat 10 wedding guests
+
+Collect one integer input and assign it to the variable "guests:.
+
+Using integer division, calculate the total number of tables that will be filled.
+
+Output the number of tables filled, ending with a newline. Ensure the program output matches the formatting below and works for a variety of input values
+
+If the input is:
+
+340
+
+The output is:
+
+Tables filled: 34
+
+If the input is:
+
+349
+
+The output is:
+
+Tables filled: 34
+ */
+
+
+package wguJavaFundamentals;
+
+//Import scanner utility to get user input
+import java.util.Scanner;
+
+public class q3WeddingTables {
+    public static void main(String[] args) {
+
+    //Create new scanner
+    Scanner sc = new Scanner(System.in);
+
+    //Declare variables
+    int guests;
+    int tableSize = 10;
+    int tablesFilled;
+
+    //Get number of guests from user input
+    System.out.println("How many guests are expected to attend the wedding?");
+    guests = sc.nextInt();
+
+    //Calculate how many filled tables there will be based on the number of guests
+    tablesFilled = guests / tableSize;
+
+    //Output the number of filled tables
+    System.out.println("Tables filled: " + tablesFilled);
+
+    }
+}
+
