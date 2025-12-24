be72c0995545d96102196c49a409c2b0f8b43fb2
ob1codeob <ob1codeob@gmail.com>
2025-12-24 15:10:12 -0800

    generated output using print function

---
+++ wguJavaFundamentals/q1LetterPatternOutput.java
@@ -0,0 +1,14 @@
+package wguJavaFundamentals;
+
+public class q1LetterPatternOutput {
+    public static void main(String[] args) {
+
+        //Output character "H" using just print
+        System.out.println("H   H");
+        System.out.println("H   H");
+        System.out.println("HHHHH");
+        System.out.println("H   H");
+        System.out.println("H   H");
+
+    }
+}
