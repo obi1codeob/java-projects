1db846d2de2f9209daf6e41886afe51f1a7db1f5
ob1codeob <ob1codeob@gmail.com>
2025-12-24 15:29:46 -0800

    output product three times on same line, using for loop

--- wguJavaFundamentals/q2Multiplication.java
+++ wguJavaFundamentals/q2Multiplication.java
@@ -26,19 +26,31 @@ public class q2Multiplication {
         product = starting_num * multiplier;
 
         //Output product
-        System.out.print(product);
+//        System.out.print(product);
 
         //Add space after product
-        System.out.print(" ");
+//        System.out.print(" ");
 
         //Output product two more time, each time multiplying product by multiplier
-        product *= multiplier;
-        System.out.print(product);
+//        product *= multiplier;
+//        System.out.print(product);
 
         //Add space after product
-        System.out.print(" ");
+//        System.out.print(" ");
+//
+//        product *= multiplier;
+//        System.out.println(product);
 
-        product *= multiplier;
-        System.out.println(product);
+        //Generate output using for loop (comment out above code to line 29 first)
+        for (int i = 0; i < 3; i++) {
+            if (i < 2) {
+                System.out.print(product + " ");
+                product *= multiplier;
+            }
+            else {
+                System.out.print(product);
+            }
+
+        }
     }
 }
