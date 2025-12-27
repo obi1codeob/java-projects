52d4ce4814d7d396dfa13f7442d5b186db8129ee
ob1codeob <ob1codeob@gmail.com>
2025-12-27 12:01:36 -0800

    created variables for number inputs and string conversions in order to display 3-digit number in output

---
+++ wguJavaFundamentals/q5_divisibleBy3.java
@@ -0,0 +1,25 @@
+package wguJavaFundamentals;
+
+import java.util.Scanner;
+
+public class q5_divisibleBy3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //Declare variables
+        // Numbers input from user
+        int hundreds;
+        int tens;
+        int ones;
+
+        //Conversions so output display three-digit number made up from number input
+        String hundredsStr;
+        String tensStr;
+        String onesStr;
+
+
+
+
+    }
+}
